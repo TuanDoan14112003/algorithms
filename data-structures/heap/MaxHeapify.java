aeafc0b0331088d9b915dbb1835808ff0100bf64
TuanDoan14112003 <tuandoan14112003@gmail.com>
2024-10-24 13:01:18 +1100

    heap: maxHeapify procedure

---
+++ data-structures/heap/MaxHeapify.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class MaxHeapify {
+	public static void main(String[] args) {
+		int[] list = new int[] {16,4,10,14,7,9,3,2,8,1};
+		maxHeapify(list, list.length, 1);
+		System.out.println(Arrays.toString(list));
+	}
+
+	public static void maxHeapify(int[] list, int heapSize, int index) {
+		if (index >= heapSize) return;
+		int largest = index;
+		int leftChild = 2*index + 1;
+		int rightChild = 2*index + 2;
+
+		if (leftChild < heapSize && list[leftChild] > list[index]) {
+			largest = leftChild;
+		} 
+		if (rightChild < heapSize && list[rightChild] > list[largest]) {
+			largest = rightChild;
+		}
+
+		// swapping list[index] and list[largest]
+		if (largest != index) {
+			int temp = list[index];
+			list[index] = list[largest];
+			list[largest] = temp;
+			maxHeapify(list, heapSize, largest);
+		}
+	} 
+}
