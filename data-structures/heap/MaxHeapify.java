c475dcfbd0a6c862ae4446a368c5ce4d608eff15
TuanDoan14112003 <tuandoan14112003@gmail.com>
2024-10-25 11:58:51 +1100

    heap: update maxHeapify description

--- data-structures/heap/MaxHeapify.java
+++ data-structures/heap/MaxHeapify.java
@@ -1,3 +1,13 @@
+// Description: The procedure maxHeapify maintains the max-heap property. Its input are an array, an integer specifying
+// the heap size and an index. When it is called, maxHeapify assumes that the binary trees rooted at left and right
+// children are max-heaps but that the index node might be smaller than its children. During the procedure, maxHeapify 
+// lets the index value float down in the max-heap so that the subtree rooted at index obeys the max heap property.
+
+// Running time: Let T(n) be the running time of maxHeapify procedure, we have:
+// T(n) <= T(2n/3) + Theta(1)
+// Therefore, T(n) = O(lg(n)) = O(h)
+// where h is the height of the heap
+
 import java.util.Arrays;
 public class MaxHeapify {
 	public static void main(String[] args) {
@@ -13,7 +23,7 @@ public class MaxHeapify {
 
 		if (leftChild < heapSize && list[leftChild] > list[index]) {
 			largest = leftChild;
-		} 
+		}
 		if (rightChild < heapSize && list[rightChild] > list[largest]) {
 			largest = rightChild;
 		}
